d286afa193ae5f502ad1920ad6cdf5d874355a6b
Guilherme Guerra <guerrinha@comum.org>
2016-06-02 08:30:08 -0300

    Setup webpack + babel

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  context: __dirname + "/app",
+  entry: {
+    javascript: "./app.js",
+    html: "./index.html"
+  },
+  output: {
+    filename: 'app.js',
+    path: __dirname + "/dist"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.html$/,
+        loader: "file?name=[name].[ext]"
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      }
+    ]
+  }
+};
